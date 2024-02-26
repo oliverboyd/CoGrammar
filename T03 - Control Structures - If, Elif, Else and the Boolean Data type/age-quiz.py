f883c9ac2ff355462e1bfd421835e5db00ea0269
Oliver <oliverjohnboyd@gmail.com>
2024-02-26 00:13:45 +0000

    Added comments

--- T03 - Control Structures - If, Elif, Else and the Boolean Data type/age-quiz.py
+++ T03 - Control Structures - If, Elif, Else and the Boolean Data type/age-quiz.py
@@ -2,9 +2,9 @@ import sys
 
 print("Please insert your age: ")
 age = input()
-try: 
+try: # we try to convert the inputted string to an integer. If there are any errors then a ValueError is returned.
     age = int(age)
-except ValueError:
+except ValueError: # in this case we return a printed error message and exit the program
     print("Please enter an integer value")
     sys.exit(0)
 if age > 100:
